8773b1b032cd5736cb62d201173b4712834666c5
Martin Borgo <martinborgo8@gmail.com>
2026-03-14 14:16:25 -0300

    [Feature] basic module structure

---
+++ __manifest__.py
@@ -0,0 +1,8 @@
+{
+    "name": "Seeds",
+    "category": "Production",
+    "author": "MuchachosDev",
+    "application": True,
+    "version": "19.0.1.0.0",
+    "summery": "A small and simple module to manage and track seeds",
+}
