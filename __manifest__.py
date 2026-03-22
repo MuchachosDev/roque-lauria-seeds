d6f638620ebd12fc2b91d02ac82c779d6ad7ccf0
Confaa <juanconfaa@gmail.com>
2026-03-22 20:07:29 -0300

    .

--- __manifest__.py
+++ __manifest__.py
@@ -4,7 +4,7 @@
     "author": "MuchachosDev",
     "application": True,
     "version": "19.0.1.0.0",
-    "summery": "A small and simple module to manage and track seed tests",
+    "summary": "A small and simple module to manage and track seed tests",
     "depends": ["base", "mail"],
-    "data": ["security/security.xml", "security/ir.model.acccess.csv"],
+    "data": ["security/security.xml", "security/ir.model.access.csv"],
 }
