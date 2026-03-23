5af8e2e08b558d3e7d84575a77a32985274c0ce5
Confaa <juanconfaa@gmail.com>
2026-03-23 20:07:07 -0300

    TEST

--- __manifest__.py
+++ __manifest__.py
@@ -20,7 +20,7 @@
         "views/menu.xml",
         # Pre-loaded data
         "data/sequence.xml",
-        "data/vegetable.xml",
-        "data/seed_experimental_result_template.xml",
+        #"data/vegetable.xml",
+        #"data/seed_experimental_result_template.xml",
     ],
 }
