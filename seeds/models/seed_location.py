ef509c6d7773e86396f778bc26dcd318c8117181
Martin Borgo <martinborgo8@gmail.com>
2026-03-31 17:30:41 -0300

    [Fix] posible fix

--- seeds/models/seed_location.py
+++ seeds/models/seed_location.py
@@ -6,6 +6,11 @@ class SeedLocation(models.Model):
     _description = "Locations"
 
     name = fields.Char(string="Nombre de la localidad")
+    company_id = fields.Many2one(
+        string='Compañía',
+        comodel_name='res.company',
+        default=lambda self: self.env.company
+    )
     state_id = fields.Many2one(
         string="Provincia",
         comodel_name="res.country.state",
