3359ebd923fa91c5ef4fcdf0a024855183f4c51c
Martin Borgo <martinborgo8@gmail.com>
2026-04-01 16:33:05 -0300

    [Fix] Invalid reference to states fixed

--- seeds/models/seed_location.py
+++ seeds/models/seed_location.py
@@ -5,19 +5,23 @@ class SeedLocation(models.Model):
     _name = "seed.location"
     _description = "Locations"
 
+    def _get_default_country(self):
+        # return self.env.user.partner_id.country_id.id
+        return self.env.ref("base.ar")
+
     name = fields.Char(string="Nombre de la localidad")
-    company_id = fields.Many2one(
-        string='Compañía',
-        comodel_name='res.company',
-        default=lambda self: self.env.company
+    country_id = fields.Many2one(
+        string="País",
+        comodel_name="res.country",
+        default=_get_default_country,
     )
     state_id = fields.Many2one(
         string="Provincia",
         comodel_name="res.country.state",
-        domain="[('country_id', '=', company_id.country_id)]"
+        domain="[('country_id', '=', country_id)]",
     )
     deperture_order_ids = fields.One2many(
         string="Ordenes de salida",
         comodel_name="seed.deperture.order",
-        inverse_name="destinatary_city"
+        inverse_name="destinatary_city",
     )
